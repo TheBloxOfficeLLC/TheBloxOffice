4bec0a60f050eb3c27814600dcfeeb3c5f107699
Michael Frieze <michaelfrieze@icloud.com>
2018-05-09 04:21:22 -0400

    Initial nodejs back-end commit with a basic readme.

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+const app = express();
+
+/* Port for Heroku or locally */
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
