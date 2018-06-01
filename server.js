7210ffe1dbfd230b478a84d209ce4c58048edf31
Michael Frieze <michaelfrieze@icloud.com>
2018-06-01 10:07:52 -0400

    Removed route on server file.

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const serveStatic = require('serve-static');
-const path = require('path');
 
 const app = express();
 
-app.use(serveStatic(path.join(__dirname, 'splash-page')));
 
 /* Port for Heroku or locally */
 const port = process.env.PORT || 5000;
